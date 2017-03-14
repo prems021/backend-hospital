c10de272d24cd73ec829923b7425aebc76fd1143
premji thankaji <premji thankaji>
2017-03-14 13:18:51 +0530

    d

--- src/routes/index.js
+++ src/routes/index.js
@@ -22,11 +22,6 @@ router.get('/apii', (req, res) => {
   });
 });
 
-router.get('/apii', (req, res) => {
-  User.find({}, (err, users) => {
-    if(err) throw err;
-    res.end(JSON.stringify(users));
-  });
-});
+
 
 export const route = router;
